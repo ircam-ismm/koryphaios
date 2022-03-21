d6471fd66bda4b2d80225b46dc58308e44565298
agolvet <alienor.golvet@gmail.com>
2022-03-21 15:46:04 +0100

    small mods

--- src/clients/player/states/Welcome.js
+++ src/clients/player/states/Welcome.js
@@ -5,7 +5,24 @@ import { html } from 'lit-html';
 export default class Welcome extends State {
   render() {
     return html`
-      <h1>${this.name}</h1>
+      <p
+        style="
+          position: absolute;
+          top: 40%;
+          left: 50%;
+          transform: translate(-50%, 0);
+          font-size: large;
+        "
+      >Welcome to the performance of ${this.context.score.get('piece')} by ${this.context.score.get('composer')}</p>
+      <p
+        style="
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, 0);
+          font-size: large;
+        "
+      >The event will begin shortly</p>
     `;
   }
 }
